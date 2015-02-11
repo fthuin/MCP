a7a11d3e24da3679bfcb534f3b45607762e5013e
LeonardJulemont <leonard.julemont@student.uclouvain.be>
2015-02-11 15:48:30 +0100

    Update CT39V104.java
    Réponses questions

--- CT39/CT39V104.java
+++ CT39/CT39V104.java
@@ -58,10 +58,18 @@ public class CT39V104 {
   
   //***********************************************************************************
   //Expliquez la notion de "programme correct"
+  //
+  // Un programme est correct s'il respecte les spécifications (pré et post-conditions) et s'il se finit. 
 
   //***********************************************************************************
   //Expliquez comment vous avez développé ce programme
+  /*
+   * Pour trouver le programme nécessaire pour résoudre ce problème, nous avons commencé par prendre un exemple de tableau. A partir de ce tableau, nous avons commencé par prendre la première case et voir quelle devait être la valeur de la somme maximale. Par la suite, nous avons ajouté la case suivante et nous avons réfléchit à ce qu'il devait ce passer pour que la variable représentant la somme maximale corresponde toujours à la somme maximale. Autrement dit, nous voulions que cette variable ait toujours la même propriété à chaque ajout de nouvelle case, jusqu'à la dernière du tableau. 
+   * Une fois avoir finis avec le premier tableau, nous avons pris un autre tableau qui contenait des situations plus critiques par rapport au premier pseudo-code que nous avions. Nous avons apporté les modifications nécessaires au programme. 
+   * Nous avons finis par écrire le programme en Java et le tester sur différents tableaux. 
+   */
   
   //***********************************************************************************
   //Expliquez pourquoi vous pensez que votre méthode est correcte
+  //Nous pensons que notre méthode est correcte car nous avons commencé par la première case du tableau puis nous avons regardé en ajoutant une nouvelle case, mais en gardant la propriété correcte. Comme nous travaillons sur un tableau qui contient un ensemble fini d'éléments, nous pouvons adopter un raisonnement inductif pour démontrer la justesse de notre algorithme. 
 }
