e4795de1e2115fb13b882d4339316c3e1caca24a
Aymeric De Cocq <aymeric.decocq@student.uclouvain.be>
2015-02-11 21:14:01 +0100

    typo

--- CT39/CT39V104.java
+++ CT39/CT39V104.java
@@ -64,12 +64,12 @@ public class CT39V104 {
   //***********************************************************************************
   //Expliquez comment vous avez développé ce programme
   /*
-   * Pour trouver le programme nécessaire pour résoudre ce problème, nous avons commencé par prendre un exemple de tableau. A partir de ce tableau, nous avons commencé par prendre la première case et voir quelle devait être la valeur de la somme maximale. Par la suite, nous avons ajouté la case suivante et nous avons réfléchit à ce qu'il devait ce passer pour que la variable représentant la somme maximale corresponde toujours à la somme maximale. Autrement dit, nous voulions que cette variable ait toujours la même propriété à chaque ajout de nouvelle case, jusqu'à la dernière du tableau. 
-   * Une fois avoir finis avec le premier tableau, nous avons pris un autre tableau qui contenait des situations plus critiques par rapport au premier pseudo-code que nous avions. Nous avons apporté les modifications nécessaires au programme. 
+   * Pour trouver le programme nécessaire pour résoudre ce problème, nous avons commencé par prendre un exemple de tableau. A partir de ce tableau, nous avons pris la première case et avons regardé quelle devait être la valeur de la séquence de somme maximale. Par la suite, nous avons ajouté la case suivante et nous avons réfléchit à ce qu'il devait ce passer pour que la variable représentant la somme maximale corresponde toujours à la somme maximale. Autrement dit, nous voulions que cette variable ait toujours la même propriété à chaque ajout de nouvelle case, jusqu'à la dernière du tableau. 
+   * Une fois avoir finis avec le premier tableau, nous avons pris un autre tableau qui contenait des situations plus critiques par rapport au premier pseudo-code que nous avions. Nous avons apporté les modifications nécessaires au programme pour répondre aux situations que nous ne traitions pas dans le cadre de notre premier tableau. 
    * Nous avons finis par écrire le programme en Java et le tester sur différents tableaux. 
    */
   
   //***********************************************************************************
   //Expliquez pourquoi vous pensez que votre méthode est correcte
-  //Nous pensons que notre méthode est correcte car nous avons commencé par la première case du tableau puis nous avons regardé en ajoutant une nouvelle case, mais en gardant la propriété correcte. Comme nous travaillons sur un tableau qui contient un ensemble fini d'éléments, nous pouvons adopter un raisonnement inductif pour démontrer la justesse de notre algorithme. 
+  //Nous pensons que notre méthode est correcte car nous avons commencé par la première case du tableau puis nous avons regardé en ajoutant une nouvelle case, tout en gardant la propriété correcte. Comme nous travaillons sur un tableau qui contient un ensemble fini d'éléments, nous pouvons adopter un raisonnement inductif pour démontrer la justesse de notre algorithme. 
 }
